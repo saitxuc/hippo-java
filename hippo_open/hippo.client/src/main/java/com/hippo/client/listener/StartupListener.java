8e3eb4f7ebeee4ca8a6c11c24c4e9129c02c921c
saitxuc <saitxuc@163.com>
2015-12-17 16:28:48 +0800

    resume push codes

--- hippo_open/hippo.client/src/main/java/com/hippo/client/listener/StartupListener.java
+++
@@ -1,7 +0,0 @@
-package com.hippo.client.listener;
-
-import com.hippo.client.transport.cluster.ClusterConnectionControl;
-
-public interface StartupListener {
-	void startup(ClusterConnectionControl clusterControl);
-}
--- hippo_open/hippo.webconsole/SQL/DDL-20150608-DROP.sql
+++
@@ -1,7 +0,0 @@
-
-
-DROP TABLE IF EXISTS `hippo_cluster_info`;
-DROP TABLE IF EXISTS `hippo_server_info`;
-DROP TABLE IF EXISTS `hippo_zk_cluster`;
-DROP TABLE IF EXISTS `hippo_zk_dataservers`;
-DROP TABLE IF EXISTS `hippo_zk_tables`;
