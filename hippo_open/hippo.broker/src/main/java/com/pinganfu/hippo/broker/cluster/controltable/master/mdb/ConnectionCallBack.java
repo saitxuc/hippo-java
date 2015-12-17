b85cd04d13284949a848255caf60e47768094c78
saitxuc <saitxuc@163.com>
2015-12-17 15:51:07 +0800

    delete file and resume push

--- hippo_open/hippo.broker/src/main/java/com/pinganfu/hippo/broker/cluster/controltable/master/mdb/ConnectionCallBack.java
+++
@@ -1,5 +0,0 @@
-package com.pinganfu.hippo.broker.cluster.controltable.master.mdb;
-
-public abstract class ConnectionCallBack {
-    public abstract void removeConnectionCallBack(String clientId);
-}
--- hippo_open/hippo.broker/src/main/java/com/pinganfu/hippo/broker/cluster/simple/client/ISlaveReplicatedClient.java
+++
@@ -1,9 +0,0 @@
-package com.pinganfu.hippo.broker.cluster.simple.client;
-
-import com.pinganfu.hippo.common.lifecycle.LifeCycleSupport;
-
-/**
- * @author yangxin
- */
-public abstract class ISlaveReplicatedClient extends LifeCycleSupport {
-}
